dd3a181c2553ed64738fe10958fc8265d220e917
Wilson Page <wilsonpage@me.com>
2017-10-15 10:34:41 +0100

    API no longer returns an artist object only artists[]

--- telegram/utils/create-message.js
+++ telegram/utils/create-message.js
@@ -3,6 +3,6 @@ module.exports = (json) => {
   switch (json.type) {
     case 'artist': return `_${json.name}_ on any music service ✨`;
     case 'album':
-    case 'track': return `"${json.name}" by ${json.artist.name} on any music service ✨`;
+    case 'track': return `"${json.name}" by ${json.artists[0].name} on any music service ✨`;
   }
 };
